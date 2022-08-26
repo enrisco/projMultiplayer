a520da6e25547c001396483472d6f1d3a01a756c
enrisco <enriquerodrigues790@gmail.com>
2022-08-25 22:19:53 -0300

    v0.0.4
    - knockback em progresso

---
+++ projMultiplayer/projMultiplayer/Assets/Porrada.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Porrada : MonoBehaviour
+{
+    [SerializeField] Collider IgnorableCollider;
+    [SerializeField] KeyCode AttackButton;
+
+    Rigidbody Rigidbody;
+
+    [SerializeField] float KnockbackStrenght;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Teste");
+        if (collision.collider != IgnorableCollider)
+        {
+            if (Input.GetKey(AttackButton))
+            {
+                if (TryGetComponent<Porrada>(out Porrada porrada)) porrada.GetDamage(transform.position);
+            }
+        }
+    }*/
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Teste 1");
+        if (other != IgnorableCollider)
+        {
+            Debug.Log("Teste 2");
+            if (Input.GetKey(AttackButton))
+            {
+                Debug.Log("Teste 3");
+                if (other.TryGetComponent<Porrada>(out Porrada porrada)) porrada.GetDamage(transform.parent.position);
+            }
+        }
+    }
+
+    public void GetDamage(Vector3 strikerPosition)
+    {
+        Debug.Log("Teste 4");
+        Vector3 direction = (strikerPosition + transform.parent.position).normalized;
+        transform.parent.GetComponent<Rigidbody>().AddForce(direction * KnockbackStrenght, ForceMode.Impulse); 
+    }
+}
